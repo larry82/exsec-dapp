71931a4caefd3870f49b9d6b39be5058aacc2b04
Larry Tu <larry820709@gmail.com>
2019-03-23 17:09:29 +0800

    feat(@dapp): build up struct for dapp

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import ExsecDapp from '@/components/exsec-dapp'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'exsec-dapp',
+      component: ExsecDapp
     }
   ]
 })
